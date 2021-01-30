b230b2411805acfbb1b94e666afc52fc98e9636f
Edwin <edwin_m_paz@hotmail.com>
2021-01-29 19:40:34 -0600

    Add route for Auth component

--- auth-ui/src/App.tsx
+++ auth-ui/src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.scss';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Auth from './auth/Auth';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path='/'>
+        <div className={style["App"]}>
+          <header className={style["App-header"]}>
+            <img src={logo} className={style["App-logo"]} alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className={style["App-link"]}
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Route>
+      <Route path='/auth'>
+        <Auth />
+      </Route>
+    </Router>
   );
 }
 
