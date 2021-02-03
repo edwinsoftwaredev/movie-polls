8ce441c5280d962c2b81c7b20636290643bc271c
Edwin <edwin_m_paz@hotmail.com>
2021-02-03 10:26:48 -0600

    Add links in Footer

--- web-app/src/footer/Footer.tsx
+++ web-app/src/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from './Footer.module.scss';
 
 const Footer: React.FC = () => {
@@ -7,6 +8,15 @@ const Footer: React.FC = () => {
 			<div className={style['app-title']}>
 				Movie Polls
 			</div>
+			<div className={style['footer-columns']}>
+        <div className={style['column']}>
+          <div className={style['header']}>App</div>
+          <div className={style['links']}>
+            <Link to='/'>About</Link>
+            <Link to='/'>Contact</Link>
+          </div>
+        </div>
+      </div>
 		</div>
 	);
 };
