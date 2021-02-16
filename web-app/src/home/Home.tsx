40cfebd799c6fb3b4271a29a9e85afab95215a63
Edwin <edwin_m_paz@hotmail.com>
2021-02-15 18:37:18 -0600

    Update popular movies query

--- web-app/src/home/Home.tsx
+++ web-app/src/home/Home.tsx
@@ -8,6 +8,8 @@ const Home: React.FC = () => {
   const [top10Movies, setTop10Movies] = useState<IMovie[]>([]);
 
   useEffect(() => {
+    const nowDate = new Date(Date.now());
+    const previousYear = new Date(nowDate.setFullYear(nowDate.getFullYear() - 1));
     Axios.get(
       `${process.env.REACT_APP_TMDB_API_URL}/genre/movie/list`,
       {
@@ -20,7 +22,7 @@ const Home: React.FC = () => {
       const getList = async () => {
         const tempList: IMovie[] = [];
         let pageNum = 1;
-        while (tempList.length < 10) {
+        while (pageNum <= 500) {
           const pageResponse = await Axios.get<IMovieRequest>(
             `${process.env.REACT_APP_TMDB_API_URL}/movie/popular`,
             {
@@ -33,9 +35,7 @@ const Home: React.FC = () => {
 
           const pageFiltered = pageResponse.data.results
             .filter(movie => (
-                movie.vote_count > 2500 &&
-                movie.vote_average >= 7 &&
-                new Date(movie.release_date) >= new Date(new Date(Date.now()).setFullYear(new Date(Date.now()).getFullYear() - 1))
+                new Date(movie.release_date) >= previousYear
               )
             )
             .map((movie, index) => (
@@ -50,10 +50,18 @@ const Home: React.FC = () => {
           pageNum++;
         }
 
-        return tempList;
+        // console.log(tempList.slice(0, 10)); <- use this to fetch the top 10 popular movies
+
+        const sortedByVoteCount = tempList
+          .filter(movie => (movie.vote_count >= 1000 && movie.vote_average >= 7))
+          .sort((a, b) => b.vote_count - a.vote_count)
+          .slice(0, 10)
+          .sort((a, b) => b.vote_average - a.vote_average);
+
+        return sortedByVoteCount;
       }
 
-      getList().then(list => setTop10Movies(list.sort((a, b) => b.vote_average - a.vote_average).slice(0, 10)))
+      getList().then(list => setTop10Movies(list));
     });
   }, []);
 
