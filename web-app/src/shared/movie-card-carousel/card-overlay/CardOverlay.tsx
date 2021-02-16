6ea60f14c9427bf35e5d6a7f8f3490ca3c4cd27e
Edwin <edwin_m_paz@hotmail.com>
2021-02-16 15:53:54 -0600

    Update CardOverlay component to display director, cast, and duration of a movie

--- web-app/src/shared/movie-card-carousel/card-overlay/CardOverlay.tsx
+++ web-app/src/shared/movie-card-carousel/card-overlay/CardOverlay.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import style from './CardOverlay.module.scss';
 import popularyIcon from '../../resources/icons/star-struck.png';
-import { IMovie } from '../../interfaces/movie-types';
+import { IMovie, IMovieDetail } from '../../interfaces/movie-types';
+import Axios, { AxiosResponse } from 'axios';
 
 interface ICardOverlay {
   card: HTMLDivElement | undefined | null,
@@ -14,10 +15,38 @@ const CardOverlay: React.FC<ICardOverlay> = (props: ICardOverlay) => {
   const [active, setActive] = useState(false);
   const [transitionEnded, setTransitionEnded] = useState(true);
   const overlayRef = useRef<HTMLDivElement>(null);
+  const [cast, setCast] = useState<string[]>([]);
+  const [duration, setDuration] = useState<string>('');
+  const [director, setDirector] = useState<string>('');
+  const [certification, setCertificaction] = useState<string>('');
 
   const handleOverlayClick = () => {
     setActive(false);
   };
+
+  useEffect(() => {
+    if (props.movie) {
+      Axios.get(`${process.env.REACT_APP_TMDB_API_URL}/movie/${props.movie.id}`,
+      {
+        params: {
+          api_key: process.env.REACT_APP_TMDB_API_KEY,
+          append_to_response: 'credits,release_dates'
+        }
+      }).then((response: AxiosResponse<IMovieDetail>) => {
+        const hours = Math.floor(response.data.runtime / 60);
+        const mins = response.data.runtime % 60;
+        setDuration(hours + 'h ' + mins + 'm')
+        setCast(response.data.credits.cast.slice(0, 4).map(value => value.name))
+        setDirector(response.data.credits.crew.filter(value => value.job === 'Director')[0].name);
+        setCertificaction(
+          response.data.release_dates.results
+            .filter(value => value.iso_3166_1 === 'US')[0]
+            .release_dates[0]
+            .certification
+        );
+      });
+    }
+  }, [props.movie]);
 
   useEffect(() => {
     if (props.card) {
@@ -88,21 +117,43 @@ const CardOverlay: React.FC<ICardOverlay> = (props: ICardOverlay) => {
               }
             >
               <div className={style['primary-info']}>
-                <div className={style['title']}>
-                  {props.movie.title} 
-                </div>
-                <div className={style['release-date']}>
-                  ({props.movie.release_date.split('-')[0]})
-                </div>
-                <div className={style['popularity']}>
-                  <img 
-                    alt='Popularity' 
-                    src={popularyIcon}
-                  /> 
-                  <span>{props.movie.vote_average*10}%</span>
+                <div className={style['header']}>
+                  <div className={style['title']}>
+                    {props.movie.title}
+                  </div>
+                  <div className={style['release-date']}>
+                   ({props.movie.release_date.split('-')[0]})
+                  </div>
+                  {
+                    certification ? (
+                      <div className={style['certification']}>
+                        {certification}
+                      </div>
+                    ) : (<div></div>)
+                  }
+                  <div className={style['popularity']}>
+                    <img 
+                      alt='Popularity' 
+                      src={popularyIcon}
+                    /> 
+                    <span>{props.movie.vote_average*10}%</span>
+                  </div>
                 </div>
                 <div className={style['overview']}>
-                  {props.movie.overview}
+                  <div className={style['description']}>
+                    {props.movie.overview}
+                  </div>
+                  <div className={style['movie-info']}>
+                    <div className={style['director']}>
+                      <span>Director: </span>{director}
+                    </div>
+                    <div className={style['cast']}>
+                      <span>Cast: </span>{cast.join(', ')}
+                    </div>
+                    <div className={style['duration']}>
+                      <span>Duration: </span>{duration}
+                    </div>
+                  </div>
                 </div>
                 <div className={style['genres']}>
                   <div>{props.movie.genre_names.join(', ')}</div>
