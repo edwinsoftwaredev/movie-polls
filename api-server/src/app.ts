b67fa89d97073afadb864603f75c2cae466ccbb2
Edwin <edwin_m_paz@hotmail.com>
2021-03-04 15:13:26 -0600

    Add support for dotenv files

--- api-server/src/app.ts
+++ api-server/src/app.ts
@@ -1,19 +1,24 @@
+import dotenv from 'dotenv';
+ 
+if (process.env.NODE_ENV !== 'production') {
+  // loads env var in .env file
+  dotenv.config();
+}
+
 import Koa from 'koa';
 import KoaLogger from 'koa-logger';
 import Router from '@koa/router';
-import {default as initControllers} from './routes/init';
+import cors from '@koa/cors';
 
+import {default as initRoutes} from './routes/init';
 
 const app = new Koa();
 const router = new Router();
 
-initControllers(router);
+initRoutes(router);
 
 app.use(KoaLogger());
+app.use(cors());
 app.use(router.routes());
 
-app.use(async ctx => {
-  ctx.body = 'Hello World!';
-});
-
-app.listen(4000);
+app.listen(process.env.PORT);
