8712f7b2eba1151f4726bc5eeba83f6d2c7fb9f0
Edwin <edwin_m_paz@hotmail.com>
2021-03-05 16:52:30 -0600

    Add csrf-token GET route

---
+++ api-server/src/routes/csrf_token.ts
@@ -0,0 +1,29 @@
+import Koa from 'koa';
+import Router from '@koa/router';
+
+// IMPORTANT!
+// always set the generic types when using Router
+// to get the AUGMENTED state and context
+const router = new Router<Koa.DefaultState, Koa.DefaultContext>({
+  methods: ['GET']
+});
+
+router.get('/csrf-token', async (
+  ctx,
+  next
+) => {
+  if (ctx.session)
+    // the CSRF middleware will look for the
+    // XSRF-Token header and compare it with 
+    // the value setted in ctx.session.csrf
+    ctx.session.csrf = ctx.csrf;
+
+  ctx.cookies.set(
+    'XSRF-TOKEN',
+    ctx.session?.csrf
+  );
+
+  ctx.status = 204;
+});
+
+export default router;
