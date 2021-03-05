5edad233160c57050155fd97f134712aace897fc
Edwin <edwin_m_paz@hotmail.com>
2021-03-04 20:26:56 -0600

    Add koa-session and koa-csrf initial configuration

--- api-server/src/app.ts
+++ api-server/src/app.ts
@@ -9,16 +9,28 @@ import Koa from 'koa';
 import KoaLogger from 'koa-logger';
 import Router from '@koa/router';
 import cors from '@koa/cors';
+import session from 'koa-session'
+import CSRF from 'koa-csrf';
 
 import {default as initRoutes} from './routes/init';
 
 const app = new Koa();
+
+app.keys = [process.env.KEY as string];
+
 const router = new Router();
 const corsConfig: cors.Options = {origin: process.env.CLIENT_ORIGIN}
 
 initRoutes(router);
 
 app.use(KoaLogger());
+app.use(session(app));
+app.use(new CSRF({
+  invalidTokenMessage: 'Invalid CSRF token',
+  invalidTokenStatusCode: 403,
+  excludedMethods: [ 'GET', 'HEAD', 'OPTIONS' ],
+  disableQuery: false
+}));
 app.use(cors(corsConfig));
 app.use(router.routes());
 
