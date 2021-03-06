2c7c0e2832115855cf169b9cdb03370d88215ae7
Edwin <edwin_m_paz@hotmail.com>
2021-03-06 17:46:26 -0600

    Replace cookie-based session with mysql-based session store

--- api-server/src/app.ts
+++ api-server/src/app.ts
@@ -10,20 +10,47 @@ import KoaLogger from 'koa-logger';
 import Router from '@koa/router';
 import cors from '@koa/cors';
 import session from 'koa-session'
+import {PrismaClient} from '@prisma/client';
 
 import {default as initRoutes} from './routes/init';
 
 const app = new Koa();
+const prisma = new PrismaClient();
 
 app.keys = [process.env.KEY as string];
 
 const router = new Router();
-const corsConfig: cors.Options = {origin: process.env.CLIENT_ORIGIN}
+const corsConfig: cors.Options = {origin: process.env.CLIENT_ORIGIN};
+
+const sessionConfig: Partial<session.opts> = {
+  store: {
+    async get(key, maxAge, data) {
+      // TODO: Errors should be catched.
+      const sessionObj = await prisma.session.findUnique({
+        where: {id: key}
+      }).finally(() => prisma.$disconnect());
+      return sessionObj?.session;
+    },
+    async set(key, sess, maxAge, data) {
+      // TODO: Errors should be catched.
+      await prisma.session.upsert({
+        where: {id: key},
+        update: {session: sess},
+        create: {id: key, session: sess}
+      }).finally(() => prisma.$disconnect());
+    },
+    async destroy(key){
+      // TODO: Errors should be catched.
+      await prisma.session.delete({where: {id: key}})
+        .finally(() => prisma.$disconnect());
+    }
+  }
+};
 
 initRoutes(router);
 
 app.use(KoaLogger());
-app.use(session(app)); // client side session -> TODO: use a proper store
+app.use(session(sessionConfig, app)); // client side session -> Use a proper store like Redis
 app.use(cors(corsConfig));
 app.use(router.routes());
 
