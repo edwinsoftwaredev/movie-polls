03c3a515eebed93e518f65f56d322324483a6d6a
Edwin <edwin_m_paz@hotmail.com>
2021-03-08 18:21:50 -0600

    Add middleware for Firebase token validation

--- api-server/src/routes/init.ts
+++ api-server/src/routes/init.ts
@@ -2,6 +2,9 @@ import Router from "@koa/router";
 import CSRF from 'koa-csrf';
 import authRouter from './auth';
 import csrfTokenRouter from './csrf_token';
+import movieRouter from './movie';
+import Koa from 'koa';
+import * as admin from 'firebase-admin';
 
 const csrfMiddleware = new CSRF({
   invalidTokenMessage: 'Invalid CSRF token',
@@ -23,12 +26,35 @@ const csrfMiddlewareGenerator = new CSRF({
   disableQuery: false
 });
 
-const init = (router: Router): void => {
+const firebaseTokenValidatorMiddleware = async (
+  ctx: Koa.ParameterizedContext<Koa.DefaultState, Koa.DefaultContext, any>,
+  next: Koa.Next
+) => {
+  const idToken = ctx.header.authorization?.split('Bearer ')[1];
+  // check if authorization header and Bearer token are present
+  if (!idToken) {
+    ctx.status = 401;
+    return;
+  }
+  
+  const idTokenDecoded = await admin.auth().verifyIdToken(idToken)
+    .catch(reason => {
+      console.warn(`Firebase IdToken validation failed. From: ${ctx.ip}, Destination: ${ctx.url}`);
+      ctx.status = 401;
+    });
+
+  if (idTokenDecoded) {
+    next();
+  }
+};
+
+const init = (router: Router<Koa.DefaultState, Koa.DefaultContext>): void => {
   // The order of this middleware list
   // is based on the level of protection determined 
   // for each route in ascending order.
   router.use('/api', csrfMiddlewareGenerator, csrfTokenRouter.routes(), csrfTokenRouter.allowedMethods());
   router.use('/api', csrfMiddleware, authRouter.routes(), authRouter.allowedMethods());
+  router.use('/api', csrfMiddleware, firebaseTokenValidatorMiddleware, movieRouter.routes(), authRouter.allowedMethods());
 };
 
 export default init;
