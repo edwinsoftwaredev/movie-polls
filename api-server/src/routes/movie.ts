14659b217629f94678e9a4c389849f2b5b590ac1
Edwin <edwin_m_paz@hotmail.com>
2021-03-10 22:46:43 -0600

    Update top-movies route

--- api-server/src/routes/movie.ts
+++ api-server/src/routes/movie.ts
@@ -1,9 +1,17 @@
 import Koa from 'koa';
 import Router from '@koa/router';
+import { AxiosError } from 'axios';
+import MoviesService from '../services/movie-service';
 
 const router = new Router<Koa.DefaultState, Koa.DefaultContext>({prefix: '/movies'});
 
-router.get('/top-movies', (ctx, next) => {
+router.get('/top-movies', async (ctx, next) => {
+  const topMovies = await MoviesService.fetchTopMovies().catch((error: AxiosError | Error) => {
+    console.error(`Error when fetching Top Movies. Message: ${error.message}`);
+    ctx.throw(500);
+  });
+
+  ctx.body = topMovies;
   ctx.status = 200;
 });
 
