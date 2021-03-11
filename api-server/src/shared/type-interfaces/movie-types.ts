cb723f172861ed7a1a93b1e87c5e37572827b92a
Edwin <edwin_m_paz@hotmail.com>
2021-03-10 22:43:27 -0600

    Add TMDB type definitios

---
+++ api-server/src/shared/type-interfaces/movie-types.ts
@@ -0,0 +1,60 @@
+export interface IMovieRequest {
+    page: number;
+    results: IMovie[];
+    total_pages: number;
+    total_results: number;
+}
+
+export interface IMovie {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    genre_names: string[]; // this is a custom property
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+    // key: number; // this is a custom property
+}
+
+export interface IMovieDetail {
+    homepage: string;
+    production_countries: {
+        iso_3166_1: string;
+        name: string
+    }[];
+    credits: {
+        cast: {
+            name: string;
+        }[];
+        crew: {
+            job: string;
+            name: string
+        }[];
+    };
+    release_dates: {
+        results: {
+            iso_3166_1: string;
+            release_dates: {
+                certification: string;
+            }[];
+        }[];
+    };
+    runtime: number;
+}
+
+export interface IGenreRequest {
+    genres: IGenre[];
+}
+
+export interface IGenre {
+    id: number;
+    name: string;
+}
