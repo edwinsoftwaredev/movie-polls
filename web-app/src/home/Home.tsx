60e47996c4ca2f19317639ccce02ee8a5dd01e02
Edwin <edwin_m_paz@hotmail.com>
2021-03-15 18:30:29 -0600

    Update Home component to use fetchTop10Movies epic

--- web-app/src/home/Home.tsx
+++ web-app/src/home/Home.tsx
@@ -1,69 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import MovieCardCarousel from '../shared/movie-card-carousel/MovieCardCarousel';
 import style from './Home.module.scss';
-import Axios, { AxiosResponse } from 'axios';
-import { IGenreRequest, IMovie, IMovieRequest } from '../shared/interfaces/movie-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { top10MoviesSelector } from '../services/slices-selectors/movies';
+import { fetchTop10Movies } from '../services/epics/movies';
 
 const Home: React.FC = () => {
-  const [top10Movies, setTop10Movies] = useState<IMovie[]>([]);
+  const top10Movies = useSelector(top10MoviesSelector);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const nowDate = new Date(Date.now());
-    const previousYear = new Date(nowDate.setFullYear(nowDate.getFullYear() - 1));
-    Axios.get(
-      `${process.env.REACT_APP_TMDB_API_URL}/genre/movie/list`,
-      {
-        params: {
-          api_key: process.env.REACT_APP_TMDB_API_KEY
-        }
-      }
-    ).then((response: AxiosResponse<IGenreRequest>) => {
-      const genres = response.data.genres;
-      const getList = async () => {
-        const tempList: IMovie[] = [];
-        let pageNum = 1;
-        while (pageNum <= 500) {
-          const pageResponse = await Axios.get<IMovieRequest>(
-            `${process.env.REACT_APP_TMDB_API_URL}/movie/popular`,
-            {
-              params: {
-                api_key: process.env.REACT_APP_TMDB_API_KEY,
-                page: pageNum
-              }
-            }
-          );
-
-          const pageFiltered = pageResponse.data.results
-            .filter(movie => (
-                new Date(movie.release_date) >= previousYear
-              )
-            )
-            .map((movie, index) => (
-              {
-                ...movie,
-                genre_names: movie.genre_ids.map(id => genres.find((genre) => genre.id === id)?.name ?? ''),
-                key: index
-              }
-            ));
-
-          tempList.push(...pageFiltered);
-          pageNum++;
-        }
-
-        // console.log(tempList.slice(0, 10)); <- use this to fetch the top 10 popular movies
-
-        const sortedByVoteCount = tempList
-          .filter(movie => (movie.vote_count >= 1000 && movie.vote_average >= 7))
-          .sort((a, b) => b.vote_count - a.vote_count)
-          .slice(0, 10)
-          .sort((a, b) => b.vote_average - a.vote_average);
-
-        return sortedByVoteCount;
-      }
-
-      getList().then(list => setTop10Movies(list));
-    });
-  }, []);
+    dispatch(fetchTop10Movies());
+  }, [dispatch]);
 
   return (
     <div className={style['home-component']}>
