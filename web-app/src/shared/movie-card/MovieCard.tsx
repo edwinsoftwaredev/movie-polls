6a8abab352685fe44d0cbd86753d6d9cf9cd1db2
Edwin <edwin_m_paz@hotmail.com>
2021-03-15 18:31:20 -0600

    Update MovieCard component

--- web-app/src/shared/movie-card/MovieCard.tsx
+++ web-app/src/shared/movie-card/MovieCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import style from './MovieCard.module.scss';
 import popularyIcon from '../resources/icons/star-struck.png';
 import { IMovie } from '../interfaces/movie-types';
@@ -12,6 +12,14 @@ interface IMovieCard {
 const MovieCard: React.FC<IMovieCard> = (props: IMovieCard) => {
   const cardReference = useRef<HTMLDivElement>(null);
   const [isMobile, setIsMobile] = useState(false);
+  const [isMovie, setIsMovie] = useState(false);
+
+  useEffect(() => {
+    const movie = props.movie;
+    if (movie.title && movie.genre_names) {
+      setIsMovie(true);
+    }
+  }, [props.movie]);
 
   useEffect(() => {
     const ref = cardReference.current;
@@ -51,46 +59,54 @@ const MovieCard: React.FC<IMovieCard> = (props: IMovieCard) => {
         style={{width: props.width + 'px', height: (props.width*9)/16 + 'px'}}
         className={style['movie-card-component']}
         onClick={e => {
-          props.handleOverlay(cardReference.current, props.movie);
-          e.stopPropagation();
+          if (isMovie) {
+            props.handleOverlay(cardReference.current, props.movie);
+            e.stopPropagation();
+          }
         }}
       >
-        <div 
-          className={style['before']} 
-          style={{
-            backgroundImage: (
-              isMobile ? 
-                `url(${process.env.REACT_APP_TMDB_API_POSTER_URL}/${props.movie.poster_path})` : 
-                `url(${process.env.REACT_APP_TMDB_API_BACKDROP_URL}/${props.movie.backdrop_path})`
-            ) 
-          }}
-        ></div>
-        <div className={style['header']}>
-          <div className={style['title']}>{props.movie.title}</div>
-          <div className={style['space']}></div>
-          <div className={style['menu-btn']} onClick={e => {
-              props.handleOverlay(cardReference.current, props.movie)
-              e.stopPropagation(); 
-            }}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        <div className={style['space']}></div>
-        <div className={style['data']}>
-          <div className={style['genres']}>
-            <div>{props.movie.genre_names.join(', ')}</div>
-          </div>
-          <div className={style['popularity']}>
-            <img 
-              alt='Populary'
-              src={popularyIcon}
-            />
-            <span> {props.movie.vote_average*10}%</span>
-          </div>
-        </div>
+        {
+          isMovie ? (
+            <Fragment>
+              <div 
+                className={style['before']} 
+                style={{
+                  backgroundImage: (
+                    isMobile ? 
+                      `url(${process.env.REACT_APP_TMDB_API_POSTER_URL}/${props.movie.poster_path})` : 
+                      `url(${process.env.REACT_APP_TMDB_API_BACKDROP_URL}/${props.movie.backdrop_path})`
+                  ) 
+                }}
+              ></div>
+              <div className={style['header']}>
+                <div className={style['title']}>{props.movie.title}</div>
+                <div className={style['space']}></div>
+                <div className={style['menu-btn']} onClick={e => {
+                    props.handleOverlay(cardReference.current, props.movie)
+                    e.stopPropagation(); 
+                  }}
+                >
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+              <div className={style['space']}></div>
+              <div className={style['data']}>
+                <div className={style['genres']}>
+                  <div>{props.movie.genre_names?.join(', ') ?? ''}</div>
+                </div>
+                <div className={style['popularity']}>
+                  <img 
+                    alt='Populary'
+                    src={popularyIcon}
+                  />
+                  <span> {props.movie.vote_average*10}%</span>
+                </div>
+              </div>
+            </Fragment>
+          ) : null
+        }
       </div>
     </div>
   )
