8fb1183f6b479e9fb07b97c56801c67a5dad79cb
Edwin <edwin_m_paz@hotmail.com>
2021-03-17 20:31:55 -0600

    Add Slider for top 10 trending movies

--- web-app/src/home/Home.tsx
+++ web-app/src/home/Home.tsx
@@ -2,20 +2,24 @@ import React, { useEffect } from 'react';
 import MovieCardCarousel from '../shared/movie-card-carousel/MovieCardCarousel';
 import style from './Home.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { top10MoviesSelector } from '../services/slices-selectors/movies';
-import { fetchTop10Movies } from '../services/epics/movies';
+import { top10PopularSelector, top10TrendingSelector } from '../services/slices-selectors/movies';
+import { fetchTop10Popular } from '../services/epics/popular-movies';
+import { fetchTop10Trending } from '../services/epics/trending-movies';
 
 const Home: React.FC = () => {
-  const top10Movies = useSelector(top10MoviesSelector);
+  const top10Popular = useSelector(top10PopularSelector);
+  const top10Trending = useSelector(top10TrendingSelector);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchTop10Movies());
+    dispatch(fetchTop10Popular());
+    dispatch(fetchTop10Trending());
   }, [dispatch]);
 
   return (
     <div className={style['home-component']}>
-      <MovieCardCarousel movieList={top10Movies} title='Top 10 Best Popular Movies'/>
+      <MovieCardCarousel movieList={top10Popular} title='Top 10 Best Popular Movies'/>
+      <MovieCardCarousel movieList={top10Trending} title='Top 10 Best Trending Movies'/>
     </div>
   );
 };
