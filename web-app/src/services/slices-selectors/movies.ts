2a584ad014d8c1437575202c40ba668ac7b40a6d
Edwin <edwin_m_paz@hotmail.com>
2021-03-17 20:28:44 -0600

    Add/Rename slices and selector (Top Popular & Top Trending)

--- web-app/src/services/slices-selectors/movies.ts
+++ web-app/src/services/slices-selectors/movies.ts
@@ -1,23 +1,33 @@
-import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSelector, createSlice, PayloadAction, Slice } from '@reduxjs/toolkit';
 import { IMovie } from '../../shared/interfaces/movie-types';
 import { RootState } from '../../store/store';
 
-const initialTop10Movies: IMovie[] = new Array(10).map((value, index) => {
+const initialMovies: IMovie[] = new Array(10).map((value, index) => {
   return {title: '', id: index} as IMovie;
 });
 
-export const top10MoviesSlice = createSlice({
-  name: 'top10Movies',
-  initialState: initialTop10Movies,
-  reducers: {
-    setTop10Movies: (state: IMovie[], action: PayloadAction<IMovie[]>) => {
-      state = action.payload;
-      return state;
+const generateSlice = (name: string): Slice<IMovie[]> => {
+  return createSlice({
+    name: name,
+    initialState: initialMovies,
+    reducers: {
+      setMovies: (state: IMovie[], action: PayloadAction<IMovie[]>) => {
+        state = action.payload;
+        return state;
+      }
     }
-  }
-});
+  });
+};
 
-export const top10MoviesSelector = createSelector(
-  (state: RootState) => state.top10Movies,
+export const top10PopularSlice = generateSlice('top10Popular');
+export const top10TrendingSlice = generateSlice('top10Trending');
+
+export const top10PopularSelector = createSelector(
+  (state: RootState) => state.top10Popular,
+  (res: IMovie[]) => res
+);
+
+export const top10TrendingSelector = createSelector(
+  (state: RootState) => state.top10Trending,
   (res: IMovie[]) => res
 );
