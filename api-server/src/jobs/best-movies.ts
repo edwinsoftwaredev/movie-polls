2a0907ac7caf2a22c675700c474dd0c86c3c085f
Edwin <edwin_m_paz@hotmail.com>
2021-03-17 20:08:57 -0600

    Update schedule job to fill Best Movies cache based on type

---
+++ api-server/src/jobs/best-movies.ts
@@ -0,0 +1,52 @@
+import prisma from '../prisma-client';
+import schedule from 'node-schedule';
+import MoviesService from '../services/movie-service';
+import { BestMoviesTypes } from '../shared/cache-movie-types';
+
+const rule = new schedule.RecurrenceRule();
+rule.minute = 0; // this will schedule to execute the job every hour at minute 0
+
+// this job is executed every hours
+export const bestMoviesJob = schedule.scheduleJob(rule, async () => {
+  console.info('Executing bestMoviesJob');
+  const today = new Date().setHours(0, 0, 0, 0);
+
+  const updateByType = async (type: BestMoviesTypes) => {
+    const cache = await prisma.bestMovies.findFirst({where: {type: type}});
+
+    if (cache) {
+      const cacheDate = cache.updateDate.setHours(0, 0, 0, 0);
+  
+      if (today - cacheDate > 0) {
+        console.info(`Updating ${type} movies cache.`);
+        const bestMovies = await MoviesService.fetchBestMoviesByType_Job(type);
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const object: any = {result: bestMovies};
+  
+        await prisma.bestMovies.update({
+          where: {id: cache.id}, 
+          data: {movies: object, updateDate: new Date(today), type: type}
+        });
+
+        console.info(`${type} movies cache updated.`);
+      }
+    } else {
+      console.log(`Caching ${type} movies.`);
+      const bestMovies = await MoviesService.fetchBestMoviesByType_Job(type);
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const object: any = {result: bestMovies};
+      const updateDate = new Date(new Date().setHours(0, 0, 0, 0));
+  
+      await prisma.bestMovies.create({
+        data: {movies: object, updateDate: updateDate, type: type}
+      });
+
+      console.log(`${type} movies cached.`);
+    }
+  }
+
+  await updateByType(BestMoviesTypes.TopMovies);
+  await updateByType(BestMoviesTypes.TrendingMovies);
+});
