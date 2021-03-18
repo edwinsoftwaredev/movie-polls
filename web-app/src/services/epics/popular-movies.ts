ddb4ef54bec8c28131b80a1bcf98114dbf049117
Edwin <edwin_m_paz@hotmail.com>
2021-03-17 20:29:56 -0600

    Update Top Popular Epic & Add Top trending Epic

---
+++ web-app/src/services/epics/popular-movies.ts
@@ -0,0 +1,40 @@
+import { Action } from "@reduxjs/toolkit";
+import { IMovie } from "../../shared/interfaces/movie-types";
+import { ActionsObservable, Epic, ofType } from 'redux-observable';
+import { Observable } from "rxjs";
+import { switchMap } from 'rxjs/operators';
+import Axios from 'axios';
+
+enum ActionTypes {
+  SAVE_TOP10POPULAR = 'top10Popular/setMovies',
+  FETCH_TOP10POPULAR = 'top10Popular/fetchTop10Popular',
+}
+
+interface ISetTop10PopularAction extends Action {
+  type: ActionTypes.SAVE_TOP10POPULAR,
+  payload: IMovie[]
+}
+
+interface IFetchTop10PopularAction extends Action {
+  type: ActionTypes.FETCH_TOP10POPULAR
+}
+
+const setTop10Popular = (movies: IMovie[]): ISetTop10PopularAction => (
+  {type: ActionTypes.SAVE_TOP10POPULAR, payload: movies}
+);
+export const fetchTop10Popular = (): IFetchTop10PopularAction => (
+  {type: ActionTypes.FETCH_TOP10POPULAR}
+);
+
+export type Top10PopularActions = ISetTop10PopularAction | IFetchTop10PopularAction;
+
+export const fetchTop10PopularEpic: Epic<Top10PopularActions> = 
+  (
+    action$: ActionsObservable<Top10PopularActions>
+  ): Observable<Top10PopularActions> => action$.pipe(
+    ofType<Top10PopularActions, IFetchTop10PopularAction>(ActionTypes.FETCH_TOP10POPULAR),
+    switchMap(async (action: IFetchTop10PopularAction) => {
+      const res = await Axios.get(`${process.env.REACT_APP_API_SERVER}/api/movies/top-movies`);
+      return setTop10Popular(res.data);
+    })
+  );
---
+++ web-app/src/services/epics/trending-movies.ts
@@ -0,0 +1,39 @@
+import { Action } from "@reduxjs/toolkit";
+import { ActionsObservable, Epic, ofType } from "redux-observable";
+import { Observable } from "rxjs";
+import { switchMap } from "rxjs/operators";
+import { IMovie } from "../../shared/interfaces/movie-types";
+import Axios from 'axios';
+
+enum ActionTypes {
+  SAVE_TOP10TRENDING = 'top10Trending/setMovies',
+  FETCH_TOP10TRENDING = 'top10Trending/fetchTop10Trending'
+}
+
+interface ISetTop10TrendingAction extends Action {
+  type: ActionTypes.SAVE_TOP10TRENDING,
+  payload: IMovie[]
+}
+
+interface IFetchTop10TrendingAction extends Action {
+  type: ActionTypes.FETCH_TOP10TRENDING
+}
+
+const setTop10Trending = (movies: IMovie[]): ISetTop10TrendingAction => (
+  {type: ActionTypes.SAVE_TOP10TRENDING, payload: movies}
+);
+export const fetchTop10Trending = (): IFetchTop10TrendingAction => (
+  {type: ActionTypes.FETCH_TOP10TRENDING}
+);
+
+export type Top10TrendingActions = ISetTop10TrendingAction | IFetchTop10TrendingAction;
+
+export const fetchTop10TrendingEpic: Epic<Top10TrendingActions> = (
+  action$: ActionsObservable<Top10TrendingActions>
+): Observable<Top10TrendingActions> => action$.pipe(
+  ofType<Top10TrendingActions, IFetchTop10TrendingAction>(ActionTypes.FETCH_TOP10TRENDING),
+  switchMap(async (action: IFetchTop10TrendingAction) => {
+    const res = await Axios.get(`${process.env.REACT_APP_API_SERVER}/api/movies/popular-movies`);
+    return setTop10Trending(res.data);
+  })
+);
