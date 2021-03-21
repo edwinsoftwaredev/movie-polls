06116d8830588046bf8aca3a3f67cf37c42b8b1a
Edwin <edwin_m_paz@hotmail.com>
2021-03-20 19:31:36 -0600

    Update App component

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -16,7 +16,7 @@ import Interceptors from './auth/security/interceptors';
 
 const InitialPage: React.FC = () => {
   const userAuthenticationStatus = useSelector(userAuthenticationStatusSelector);
-  
+
   if (userAuthenticationStatus === USER_AUTHENTICATION_STATUS.NOT_FETCHED) {
     return <LoadingPage />;
   } else if (userAuthenticationStatus === USER_AUTHENTICATION_STATUS.NOT_SIGNED) {
