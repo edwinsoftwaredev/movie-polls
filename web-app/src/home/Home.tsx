40b61ee4955e1b9e91779a6180d6ee8879e0c15a
Edwin <edwin_m_paz@hotmail.com>
2021-03-20 19:32:43 -0600

    Add Now Playing slider

--- web-app/src/home/Home.tsx
+++ web-app/src/home/Home.tsx
@@ -1,26 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import MovieCardCarousel from '../shared/movie-card-carousel/MovieCardCarousel';
 import style from './Home.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { top10PopularSelector, top10TrendingSelector } from '../services/slices-selectors/movies';
+import { nowPlayingSelector, top10PopularSelector, top10TrendingSelector } from '../services/slices-selectors/movies';
 import { fetchTop10Popular } from '../services/epics/popular-movies';
 import { fetchTop10Trending } from '../services/epics/trending-movies';
+import { fetchNowPlaying } from '../services/epics/now-playing-movies';
 
 const Home: React.FC = () => {
   const top10Popular = useSelector(top10PopularSelector);
   const top10Trending = useSelector(top10TrendingSelector);
+  const nowPlaying = useSelector(nowPlayingSelector);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchTop10Popular());
     dispatch(fetchTop10Trending());
+    dispatch(fetchNowPlaying());
   }, [dispatch]);
 
   return (
-    <div className={style['home-component']}>
-      <MovieCardCarousel movieList={top10Popular} title='Top 10 Best Popular Movies'/>
-      <MovieCardCarousel movieList={top10Trending} title='Top 10 Best Trending Movies'/>
-    </div>
+    <Fragment>
+      <div id='home-component' className={style['home-component']}>
+        <MovieCardCarousel movieList={top10Popular} title='Top 10 Best Popular Movies'/>
+        <MovieCardCarousel movieList={top10Trending} title='Top 10 Best Trending Movies'/>
+        <MovieCardCarousel movieList={nowPlaying} title='Now Playing' />
+      </div>
+    </Fragment>
   );
 };
 
