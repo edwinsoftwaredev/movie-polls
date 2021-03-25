2fed7376e0499b7bdc71b87a446474f3f080a865
Edwin <edwin_m_paz@hotmail.com>
2021-03-24 19:20:43 -0600

    Add Trending movies epic

--- web-app/src/services/epics/trending-movies.ts
+++ web-app/src/services/epics/trending-movies.ts
@@ -2,12 +2,14 @@ import { Action } from "@reduxjs/toolkit";
 import { ActionsObservable, Epic, ofType } from "redux-observable";
 import { Observable } from "rxjs";
 import { switchMap } from "rxjs/operators";
-import { IMovie } from "../../shared/interfaces/movie-types";
+import { IMovie, IMoviesByGenre } from "../../shared/interfaces/movie-types";
 import Axios from 'axios';
 
 enum ActionTypes {
   SAVE_TOP10TRENDING = 'top10Trending/setMovies',
-  FETCH_TOP10TRENDING = 'top10Trending/fetchTop10Trending'
+  FETCH_TOP10TRENDING = 'top10Trending/fetchTop10Trending',
+  SAVE_TRENDING = 'trending/setTrending',
+  FETCH_TRENDING = 'trending/fetchTrending'
 }
 
 interface ISetTop10TrendingAction extends Action {
@@ -37,3 +39,32 @@ export const fetchTop10TrendingEpic: Epic<Top10TrendingActions> = (
     return setTop10Trending(res.data);
   })
 );
+
+interface ISetTrendingAction extends Action {
+  type: ActionTypes.SAVE_TRENDING,
+  payload: IMoviesByGenre[]
+}
+
+interface IFetchTrendingAction extends Action {
+  type: ActionTypes.FETCH_TRENDING
+}
+
+const setTrending = (trending: IMoviesByGenre[]): ISetTrendingAction => (
+  {type: ActionTypes.SAVE_TRENDING, payload: trending}
+);
+
+export const fetchTrending = (): IFetchTrendingAction => (
+  {type: ActionTypes.FETCH_TRENDING}
+);
+
+export type TrendingActions = ISetTrendingAction | IFetchTrendingAction;
+
+export const fetchTrendingEpic: Epic<TrendingActions> = (
+  action$: ActionsObservable<TrendingActions>
+): Observable<TrendingActions> => action$.pipe(
+  ofType<TrendingActions, IFetchTrendingAction>(ActionTypes.FETCH_TRENDING),
+  switchMap(async (action: IFetchTrendingAction) => {
+    const res = await Axios.get<IMoviesByGenre[]>(`${process.env.REACT_APP_API_SERVER}/api/movies/trending-movies`);
+    return setTrending(res.data)
+  })
+)
