b847ad6958efc395f80026fc53ac3dff69543b1b
Edwin <edwin_m_paz@hotmail.com>
2021-03-26 17:01:18 -0600

    Add FiltersBar component

---
+++ web-app/src/shared/filters-bar/FiltersBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import style from './FiltersBar.module.scss';
+
+interface IFiltersBar {
+  callbackYear: (year: string) => void 
+}
+
+const initYears = [...new Array(25)].map((value, index) => new Date().getFullYear() - index);
+
+const FiltersBar: React.FC<IFiltersBar> = (props: IFiltersBar) => {
+
+  const handleYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    props.callbackYear(event.target.value);
+  }
+
+  return (
+    <div className={style['filters-bar-container']}>
+      <div className={style['filters-container']}>
+        <div className={style['year-filter']}>
+          <label htmlFor='year-filter'>Year </label>
+          <select id='year-filter' onChange={handleYear}>
+            {
+              initYears.map(year => <option key={year} value={year}>{year}</option>)
+            }
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FiltersBar;
