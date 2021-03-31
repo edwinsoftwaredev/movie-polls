594dad30bab3fb5db6140486753e5fa956af3766
Edwin <edwin_m_paz@hotmail.com>
2021-03-30 18:42:18 -0600

    Update TopMovies component to support year filter

--- web-app/src/top-movies/TopMovies.tsx
+++ web-app/src/top-movies/TopMovies.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTopMovies } from '../services/epics/top-movies';
-import { topMoviesSelector } from '../services/slices-selectors/top-movies';
+import { setInitialTopMovies, topMoviesSelector } from '../services/slices-selectors/top-movies';
 import FiltersBar from '../shared/filters-bar/FiltersBar';
 import MovieCarousel from '../shared/movie-card-carousel/MovieCardCarousel';
 import style from './TopMovies.module.scss';
@@ -9,9 +9,12 @@ import style from './TopMovies.module.scss';
 const TopMovies: React.FC = () => {
   const dispatch  = useDispatch();
   const topMovies = useSelector(topMoviesSelector);
-  const [year, setYear] = useState(new Date().getFullYear().toString());
+  const currentYear = new Date().getFullYear().toString();
+  const initYear = topMovies.filters.year ? topMovies.filters.year : currentYear;
+  const [year, setYear] = useState(initYear);
 
   const yearFilterClbk = (year: string) => {
+    dispatch(setInitialTopMovies());
     setYear(year);
   }
 
@@ -22,10 +25,10 @@ const TopMovies: React.FC = () => {
   return (
     <div className={style['topmovies-component']}>
       <div id='sliders-container' className={style['sliders-container']}>
-        <FiltersBar callbackYear={yearFilterClbk}/>
+        <FiltersBar callbackYear={yearFilterClbk} intialYear={topMovies.filters.year}/>
         {
-          topMovies.map((item, idx) => (
-            <Fragment key={idx}>
+          topMovies.moviesByGenres.map((item, idx) => (
+            <Fragment key={idx + '-' + topMovies.filters.year}>
               <MovieCarousel title={item.genre_name} movieList={item.results}/>
             </Fragment>
           ))
