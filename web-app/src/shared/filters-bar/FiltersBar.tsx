23f7a0b578a085f560f0290ec41960f97fbf986a
Edwin <edwin_m_paz@hotmail.com>
2021-03-30 18:43:16 -0600

    Add initialYear prop to FilterBar component

--- web-app/src/shared/filters-bar/FiltersBar.tsx
+++ web-app/src/shared/filters-bar/FiltersBar.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import style from './FiltersBar.module.scss';
 
 interface IFiltersBar {
-  callbackYear: (year: string) => void 
+  callbackYear: (year: string) => void;
+  intialYear: string;
 }
 
 const initYears = [...new Array(25)].map((value, index) => new Date().getFullYear() - index);
@@ -18,7 +19,7 @@ const FiltersBar: React.FC<IFiltersBar> = (props: IFiltersBar) => {
       <div className={style['filters-container']}>
         <div className={style['year-filter']}>
           <label htmlFor='year-filter'>Year </label>
-          <select id='year-filter' onChange={handleYear}>
+          <select id='year-filter' onChange={handleYear} defaultValue={props.intialYear}>
             {
               initYears.map(year => <option key={year} value={year}>{year}</option>)
             }
