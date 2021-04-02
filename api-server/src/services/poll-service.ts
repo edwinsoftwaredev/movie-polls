1368d47e9a27faec354b4a3962ede94648909c14
Edwin <edwin_m_paz@hotmail.com>
2021-04-02 17:42:36 -0600

    Update createPoll

--- api-server/src/services/poll-service.ts
+++ api-server/src/services/poll-service.ts
@@ -1,14 +1,15 @@
-import { Poll, PollInput } from "../shared/type-interfaces/movie-poll-types";
+import { getPollVMFromPoll, PollInput, PollVM } from "../shared/type-interfaces/movie-poll-types";
 import prisma from '../prisma-client';
 import { Prisma } from ".prisma/client";
+import { getMovieFromMovieDetails } from "../shared/type-interfaces/movie-types";
+import MoviesService from "./movie-service";
 
 export default class PollService {
-	static async createPoll(poll: Poll, userId: string): Promise<Poll> {
+	static async createPoll(poll: PollVM, userId: string): Promise<PollVM> {
 		const initialMovies: Prisma.MoviePollCreateWithoutPollInput[] = poll.movies.map(movie => ({
 			movieId: movie.movieId,
 			voteCount: movie.voteCount
 		}));
-
 
 		const data: PollInput = {
 			name: poll.name,
@@ -22,6 +23,22 @@ export default class PollService {
 			include: {movies: true, tokens: true}
 		});
 
-		return res;
+		const movieDetails = await MoviesService.fetchMovieDetails(res.movies[0].movieId);
+		const movie = getMovieFromMovieDetails(movieDetails);
+
+		const pollVM = getPollVMFromPoll(res);
+		const pollResult: PollVM = {
+			...pollVM,
+			movies: res.movies.map(item => ({
+				movieId: item.movieId,
+				voteCount: item.voteCount,
+				movie: movie
+			}))
+		};
+		console.log(movie);
+
+		console.log(pollResult);
+
+		return pollResult;
 	}
 }
