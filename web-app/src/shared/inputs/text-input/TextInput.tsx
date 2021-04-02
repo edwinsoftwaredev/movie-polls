635c04e9138ea8ce066cc1c6f6aa34701355ad6f
Edwin <edwin_m_paz@hotmail.com>
2021-04-01 21:25:25 -0600

    Add onChange handler to input in TextInput component

--- web-app/src/shared/inputs/text-input/TextInput.tsx
+++ web-app/src/shared/inputs/text-input/TextInput.tsx
@@ -5,12 +5,19 @@ interface ITextInput {
   name: string;
   placeholder: string;
   otherProperties: object;
+  clbk: (value: string) => void
 }
 
 const TextInput: React.FC<ITextInput> = (props: ITextInput) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.clbk(e.target.value);
+  };
+
   return (
     <div className={style['text-input-container']}>
       <input
+        type='text'
+        onChange={handleChange}
         name={props.name}
         placeholder={props.placeholder}
         {...props.otherProperties}
