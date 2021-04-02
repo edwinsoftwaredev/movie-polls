ad2d8d47aaa0612c59f53381cc26b81f5d3c87e1
Edwin <edwin_m_paz@hotmail.com>
2021-04-02 17:43:31 -0600

    Update poll POST route to return a PollVM object and not a Poll object from primsa types

--- api-server/src/routes/poll.ts
+++ api-server/src/routes/poll.ts
@@ -1,12 +1,12 @@
 import Koa from 'koa';
 import Router from '@koa/router';
 import PollService from '../services/poll-service';
-import { Poll } from '../shared/type-interfaces/movie-poll-types';
+import { PollVM } from '../shared/type-interfaces/movie-poll-types';
 
 const router = new Router<Koa.DefaultState, Koa.DefaultContext>({prefix: '/polls'});
 
 router.post('/poll', async (ctx, next) => {
-  const poll: Poll = ctx.request.body;
+  const poll: PollVM = ctx.request.body;
   const res = await PollService.createPoll(poll, ctx.userId).catch((err: Error) => {
     console.log(`There was an error when createPoll was executed. Message: ${err.message}`);
     ctx.throw(500);
