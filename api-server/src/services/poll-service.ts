210196aa7639a0026ac4616e47ac4288cd495b5f
Edwin <edwin_m_paz@hotmail.com>
2021-04-05 14:41:42 -0600

    Add getPollsByUserAndOpenedStatus function in PollService

--- api-server/src/services/poll-service.ts
+++ api-server/src/services/poll-service.ts
@@ -1,10 +1,16 @@
-import { getPollVMFromPoll, PollInput, PollVM } from "../shared/type-interfaces/movie-poll-types";
+import { getPollVMFromPoll, Poll, PollInput, PollVM } from "../shared/type-interfaces/movie-poll-types";
 import prisma from '../prisma-client';
 import { Prisma } from ".prisma/client";
 import { getMovieFromMovieDetails } from "../shared/type-interfaces/movie-types";
 import MoviesService from "./movie-service";
 
 export default class PollService {
+  /**
+   * Creates a poll submitted by a user.
+   * @param poll the poll to be store in database
+   * @param userId the id of the user who is submitting the poll
+   * @returns a Promise of PollVM
+   */
 	static async createPoll(poll: PollVM, userId: string): Promise<PollVM> {
 		const initialMovies: Prisma.MoviePollCreateWithoutPollInput[] = poll.movies.map(movie => ({
 			movieId: movie.movieId,
@@ -35,10 +41,43 @@ export default class PollService {
 				movie: movie
 			}))
 		};
-		console.log(movie);
-
-		console.log(pollResult);
 
 		return pollResult;
+	}
+
+  /**
+   * Gets the open polls of a user.
+   * @param userId the id of the user who created the polls
+   * @returns a Promise of a list of open polls
+   */
+	static async getPollsByUserAndOpenedStatus(userId: string, opened: boolean): Promise<PollVM[]> {
+		const res = await prisma.poll.findMany({
+      where: {userId: userId, AND: {isOpen: true}},
+      include: {movies: true, tokens: true}
+    });
+
+    /*
+     * This is how to make calls to asyncronous functions nested in 
+     * Array.map(). Just wrap "map()" function with "await Promise.all()"
+     */
+    const polls = await Promise.all(res.map(async item => {
+      const pollVM = getPollVMFromPoll(item);
+
+      return {
+        ...pollVM,
+        movies: await Promise.all(item.movies.map(async movie => {
+          const movieDetails = await MoviesService.fetchMovieDetails(movie.movieId);
+          const movieObj = getMovieFromMovieDetails(movieDetails);
+
+          return {
+            movieId: movie.movieId,
+            voteCount: movie.voteCount,
+            movie: movieObj
+          };
+        }))
+      };
+    }));
+
+    return polls;
 	}
 }
