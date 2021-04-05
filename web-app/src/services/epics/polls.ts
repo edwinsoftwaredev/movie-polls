c46cac5e4979b0b2cc57d4b6133cdcc319af5072
Edwin <edwin_m_paz@hotmail.com>
2021-04-05 14:45:30 -0600

    Add fetchPollsEpic Epic

--- web-app/src/services/epics/polls.ts
+++ web-app/src/services/epics/polls.ts
@@ -1,14 +1,16 @@
 import { Action } from "@reduxjs/toolkit";
 import { ActionsObservable, Epic, ofType } from "redux-observable";
 import { Observable } from "rxjs";
-import { concatMap } from "rxjs/operators";
+import { concatMap, switchMap } from "rxjs/operators";
 import { IPoll } from "../../shared/interfaces/movie-poll-types";
 import PollService from "../poll-service";
 
 enum ActionTypes {
   ADD_POLL = 'polls/addPoll', // EPIC
   SET_POLL = 'polls/setPoll', // Reducer
-  FETCH_POLLS = 'polls/fetchPolls'
+  FETCH_POLLS = 'polls/fetchPolls',
+  SET_OPENED_POLLS = 'polls/setOpenedPolls',
+  SET_NOT_OPENED_POLLS = 'polls/setNotOpenedPolls'
 }
 
 interface IAddPollAction extends Action {
@@ -22,7 +24,18 @@ interface ISetPollAction extends Action {
 }
 
 interface IFetchPollsAction extends Action {
-  type: ActionTypes.FETCH_POLLS
+  type: ActionTypes.FETCH_POLLS;
+  payload: boolean;
+}
+
+interface ISetOpenedPolls extends Action {
+  type: ActionTypes.SET_OPENED_POLLS,
+  payload: IPoll[]
+}
+
+interface ISetNotOpenedPolls extends Action {
+  type: ActionTypes.SET_NOT_OPENED_POLLS,
+  payload: IPoll[]
 }
 
 export const setPoll = (poll: IPoll): ISetPollAction => ({
@@ -35,11 +48,26 @@ const addPoll = (poll: IPoll): IAddPollAction => ({
   payload: poll
 });
 
-export const fetchPolls = (): IFetchPollsAction => ({
-  type: ActionTypes.FETCH_POLLS
+export const fetchPolls = (opened: boolean): IFetchPollsAction => ({
+  type: ActionTypes.FETCH_POLLS,
+  payload: opened
 });
 
-export type PollActionTypes = IFetchPollsAction | IAddPollAction | ISetPollAction;
+const setOpenedPolls = (polls: IPoll[]): ISetOpenedPolls => ({
+  type: ActionTypes.SET_OPENED_POLLS,
+  payload: polls
+});
+
+const setNotOpenedPolls = (polls: IPoll[]): ISetNotOpenedPolls => ({
+  type: ActionTypes.SET_NOT_OPENED_POLLS,
+  payload: polls
+});
+
+export type PollActionTypes = IFetchPollsAction | 
+  IAddPollAction | 
+  ISetPollAction | 
+  ISetOpenedPolls | 
+  ISetNotOpenedPolls;
 
 // Use switchMap when getting data(read)
 // Use either mergeMap or concatMap when posting data(write)
@@ -55,3 +83,13 @@ export const setPollEpic: Epic<PollActionTypes> = (
     return addPoll(res);
   })
 );
+
+export const fetchPollsEpic: Epic<PollActionTypes> = (
+  action$: ActionsObservable<PollActionTypes>
+): Observable<PollActionTypes> => action$.pipe(
+  ofType<PollActionTypes, IFetchPollsAction>(ActionTypes.FETCH_POLLS),
+  switchMap(async (action: IFetchPollsAction) => {
+    const polls = await PollService.getPolls(action.payload);
+    return action.payload ? setOpenedPolls(polls) : setNotOpenedPolls(polls)
+  })
+)
