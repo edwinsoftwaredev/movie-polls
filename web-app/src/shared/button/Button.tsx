d245974b19b8ad01ea7dac131485750ca2ebc678
Edwin <edwin_m_paz@hotmail.com>
2021-04-07 19:28:14 -0600

    Update Button component

--- web-app/src/shared/button/Button.tsx
+++ web-app/src/shared/button/Button.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import Spinner from '../spinners/Spinners';
 import style from './Button.module.scss';
 
 interface IButton {
   name: string;
   type: 'button' | 'submit';
   classType: 'default' | 'contained';
+  spinnered: boolean
 }
 
 const Button: React.FC<IButton> = (props: IButton) => {
   return (
     <div className={style['button-container']}>
       <button className={style[props.classType]} type={props.type}>
-        {props.name}
+        {
+          !props.spinnered ? props.name : <Spinner />
+        }
       </button>
     </div>
   )
