0f8fff93ce4beafe9286e538d82b7bd511a13ed3
Edwin <edwin_m_paz@hotmail.com>
2021-04-07 19:26:40 -0600

    Add initial Spinners component

---
+++ web-app/src/shared/spinners/Spinners.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Spinners.module.scss';
+
+const Spinner: React.FC = () => {
+  return (
+  <div 
+    className={style['spinner']} 
+  >
+  </div>
+  );
+};
+
+export default Spinner;
