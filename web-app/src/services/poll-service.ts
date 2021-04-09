c26e9aa952fd0d0219cd58d805d17103f74d3d8f
Edwin <edwin_m_paz@hotmail.com>
2021-04-08 18:26:24 -0600

    Remove parameter in function getPolls

--- web-app/src/services/poll-service.ts
+++ web-app/src/services/poll-service.ts
@@ -11,12 +11,11 @@ export default class PollService {
     return res.data;
   }
 
-  static async getPolls(opened: boolean): Promise<IPoll[]> {
+  static async getPolls(): Promise<IPoll[]> {
     const res = await Axios.get<IPoll[]>(
-      `${process.env.REACT_APP_API_SERVER}/api/polls/my-polls`,
-      {params: {opened: opened}}
+      `${process.env.REACT_APP_API_SERVER}/api/polls/my-polls`
     );
-
+    
     return res.data;
   }
 
