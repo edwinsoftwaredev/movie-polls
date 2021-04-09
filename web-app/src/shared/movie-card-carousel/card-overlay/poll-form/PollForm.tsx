9608974a6609007464f6dfeb587bd9494cc629ea
Edwin <edwin_m_paz@hotmail.com>
2021-04-08 18:36:43 -0600

    Add Spinner component to PollForm component that is shown when polls being fetched

--- web-app/src/shared/movie-card-carousel/card-overlay/poll-form/PollForm.tsx
+++ web-app/src/shared/movie-card-carousel/card-overlay/poll-form/PollForm.tsx
@@ -4,6 +4,7 @@ import { setPoll } from '../../../../services/epics/polls';
 import { pollsSelector } from '../../../../services/slices-selectors/polls';
 import Button from '../../../button/Button';
 import TextInput from '../../../inputs/text-input/TextInput';
+import Spinner from '../../../spinners/Spinners';
 import style from './PollForm.module.scss';
 
 interface IPollForm {
@@ -17,6 +18,7 @@ const PollForm: React.FC<IPollForm> = (props: IPollForm) => {
   const polls = useSelector(pollsSelector);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     setSubmitted(true);
     dispatch(setPoll({
       name: name,
@@ -24,7 +26,6 @@ const PollForm: React.FC<IPollForm> = (props: IPollForm) => {
         movieId: props.movieId
       }]
     }));
-    e.preventDefault();
   };
 
   useEffect(() => {
@@ -36,27 +37,33 @@ const PollForm: React.FC<IPollForm> = (props: IPollForm) => {
       <div className={style['header']}>
         Create a New Poll
       </div>
-      <form
-        className={style['new-poll-form']}
-        onSubmit={e => submitted ? null : handleSubmit(e)}
-      >
-        <TextInput
-          name={'poll-name'}
-          clbk={value => setName(value)}
-          placeholder={'Type the name of the new poll'}
-          otherProperties={{
-            required: 'required',
-            autoComplete: 'off',
-            pattern: '[0-9A-Za-z\\s]+'
-          }}
-        />
-        <Button
-          name={'Add To A New Poll'}
-          type={"submit"}
-          classType={"default"}
-          spinnered={submitted}
-        />
-      </form>
+      {
+        !polls ? (
+          <Spinner />
+        ) : (
+          <form
+            className={style['new-poll-form']}
+            onSubmit={e => submitted ? e.preventDefault() : handleSubmit(e)}
+          >
+            <TextInput
+              name={'poll-name'}
+              clbk={value => setName(value)}
+              placeholder={'Type the name of the new poll'}
+              otherProperties={{
+                required: 'required',
+                autoComplete: 'off',
+                pattern: '[0-9A-Za-z\\s]+'
+              }}
+            />
+            <Button
+              name={'Add To A New Poll'}
+              type={"submit"}
+              classType={"default"}
+              spinnered={submitted}
+            />
+          </form>
+        )
+      }
     </Fragment>
   );
 };
