5535965308fd41b4515ceb05dbeeb24d6757b333
Edwin <edwin_m_paz@hotmail.com>
2021-04-12 17:38:51 -0600

    Fix mutation in removeMovie reducer

--- web-app/src/services/slices-selectors/polls.ts
+++ web-app/src/services/slices-selectors/polls.ts
@@ -5,6 +5,8 @@ import { RootState } from "../../store/store";
 // The value null represents the -> intentional <- absence of any object value.
 const initialState: IPoll[] | null = null; 
 
+// Notes about how to update or modify the state in a reducer
+// or how to copy objects o update them.
 export const pollsSlice = createSlice<IPoll[] | null, SliceCaseReducers<IPoll[] | null>>({
   name: 'polls',
   initialState: initialState,
@@ -34,14 +36,37 @@ export const pollsSlice = createSlice<IPoll[] | null, SliceCaseReducers<IPoll[] 
     },
     removeMovie: (state: IPoll[] | null, action: PayloadAction<IRemoveMovie>) => {
       if (state) {
+        // if an inner object in the state must be changed
+        // create a variable like the follow:
         state = state.map(poll => {
           if (poll.id === action.payload.pollId) {
-            poll.movies = 
-              poll.movies.filter(movie => movie.movieId !== action.payload.movieId)
+            // DO NOT do things like:
+            //
+            // poll.movie = something;
+            //
+            // That will MUTATE the object.
+            // To avoid mutate the state create a NEW Poll object.
+            //
+            // DO NOT use the spread operator when any of the properties will not be changed, like:
+            // {...object} <-- It doesn't change any property, therefore a shallowed copy is
+            // returned, which means that a memory address is COPIED and NOT created.
+            // {...object} == object <-- True: same memory address/same object.
+            //
+            // To change the state a NEW object has to be assigned to it.
+            // By doing that IMMUTABILITY is preserve.
+            // AVOID MUTABILITY.
+
+            // The following code CREATES a NEW object using the spread operator
+            // (the property "movies" is setted).
+
+            poll = {
+              ...poll, 
+              movies: poll.movies.filter(movie => movie.movieId !== action.payload.movieId)
+            };
           }
 
           return poll;
-        }).filter(poll => poll.movies.length !== 0)
+        }).filter(poll => poll.movies.length !== 0);
       }
 
       return state;
