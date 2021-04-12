4eedaa8bd257e7d938ea8454cfabb24720928ad4
Edwin <edwin_m_paz@hotmail.com>
2021-04-12 17:44:12 -0600

    Update Spinner component to add it background color property

--- web-app/src/shared/spinners/Spinners.tsx
+++ web-app/src/shared/spinners/Spinners.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import style from './Spinners.module.scss';
 
-const Spinner: React.FC = () => {
+interface ISpinner {
+  color: 'red' | 'white';
+}
+
+const Spinner: React.FC<ISpinner> = (props: ISpinner) => {
   return (
   <div 
     className={style['spinner']} 
+    style={{backgroundColor: props.color}}
   >
   </div>
   );
