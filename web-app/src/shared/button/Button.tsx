a02e9f208ba58f05188ef19f17bc742b4170b070
Edwin <edwin_m_paz@hotmail.com>
2021-04-13 17:54:27 -0600

    Update Button component

--- web-app/src/shared/button/Button.tsx
+++ web-app/src/shared/button/Button.tsx
@@ -6,19 +6,28 @@ interface IButton {
   name: string;
   type: 'button' | 'submit';
   classType: 'default' | 'contained' | 'radial';
-  spinnered: boolean
+  spinnered: boolean;
+  spinnerColor?: 'white' | 'red';
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 const Button: React.FC<IButton> = (props: IButton) => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    // calcback should only be called when type is button and not submit or any other
+    if (props.onClick && props.type === 'button' && !props.spinnered)
+      props.onClick(e);
+  };
+
   return (
     <div className={style['button-container']}>
       <button 
         className={style[props.classType] + ' ' + (props.spinnered ? style['inactive'] : '')} 
         type={props.type}
         disabled={props.spinnered}
+        onClick={handleClick}
       >
         {
-          !props.spinnered ? props.name : <Spinner color={'white'}/>
+          !props.spinnered ? props.name : <Spinner color={props.spinnerColor ? props.spinnerColor : 'white'}/>
         }
       </button>
     </div>
