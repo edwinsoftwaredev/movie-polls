3bdc2c5af5d86ee4228cd47a6693114e2894c2d3
Edwin <edwin_m_paz@hotmail.com>
2021-04-14 19:26:45 -0600

    Add PollCardContainer component to Home component

--- web-app/src/home/Home.tsx
+++ web-app/src/home/Home.tsx
@@ -6,6 +6,8 @@ import { nowPlayingSelector, top10PopularSelector, top10TrendingSelector } from 
 import { fetchTop10Popular } from '../services/epics/popular-movies';
 import { fetchTop10Trending } from '../services/epics/trending-movies';
 import { fetchNowPlaying } from '../services/epics/now-playing-movies';
+import { pollsSelector } from '../services/slices-selectors/polls';
+import PollCardContainer from '../shared/poll-card-container/PollCardContainer';
 
 const Home: React.FC = () => {
   const top10Popular = useSelector(top10PopularSelector);
@@ -22,6 +24,7 @@ const Home: React.FC = () => {
   return (
     <Fragment>
       <div id='sliders-container' className={style['sliders-container']}>
+        <PollCardContainer status={'current'}/>
         <MovieCardCarousel movieList={top10Popular} title='Top 10 Best Popular Movies'/>
         <MovieCardCarousel movieList={top10Trending} title='Top 10 Best Trending Movies'/>
         <MovieCardCarousel movieList={nowPlaying} title='Now Playing' />
