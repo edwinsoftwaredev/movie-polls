59a89c52a6cc73f1ba1b2602be9a020e64952cf4
Edwin <edwin_m_paz@hotmail.com>
2021-04-23 18:46:51 -0600

    Update Poll component

--- web-app/src/poll/Poll.tsx
+++ web-app/src/poll/Poll.tsx
@@ -1,11 +1,22 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router';
 import MovieService from '../services/movie-service';
 import { pollsSelector } from '../services/slices-selectors/polls';
 import { IPoll } from '../shared/interfaces/movie-poll-types';
 import { IMovie } from '../shared/interfaces/movie-types';
 import style from './Poll.module.scss';
+import {ReactComponent as EditVector} from '../shared/resources/vectors/edit-3.svg';
+import {ReactComponent as SaveVector} from '../shared/resources/vectors/save.svg';
+import Spinner from '../shared/spinners/Spinners';
+import { deleteMovie } from '../services/epics/polls';
+
+enum VOTE_STATUS {
+  NOT_FETCHED,
+  VOTED,
+  NOT_VOTED,
+  VOTING
+}
 
 const getId = (search: string): number | null => {
   const value = Number.parseInt(new URLSearchParams(search).get('id') ?? ''); 
@@ -30,44 +41,133 @@ const ProgressBar: React.FC<{
   );
 };
 
+const PollNameInput: React.FC<{
+  onChange: (value: string) => void, 
+  init: string,
+  updatable: boolean
+}> = ({onChange, init, updatable}) => {
+  const [editable, setEditable] = useState(false);
+  const [initVal, setInitVal] = useState(init);
+
+  useEffect(() => {
+    if (!editable && initVal !== init) {
+      onChange(initVal);
+    }
+  }, [initVal, onChange, editable, init]);
+  
+  return (
+    <div className={style['text-input-component']}>
+      <form onSubmit={e => {setEditable(false); e.preventDefault();} }>
+        <input 
+          type='text' 
+          onChange={e => setInitVal(e.target.value)} 
+          value={initVal}
+          disabled={!editable && updatable}
+        />
+        {
+          updatable ? (
+            <button 
+              type='button' 
+              title={editable ? 'Save' : 'Edit Title'}
+              className={style['edit-vector']} 
+              onClick={e => setEditable(state => !state)}>
+              {
+                editable ? <SaveVector /> : <EditVector /> 
+              } 
+            </button>
+          ) : null
+        }
+      </form>
+    </div>
+  )
+}
+
 const Header: React.FC<{poll: IPoll}> = ({poll}) => {
+  const [pollName, setPollName] = useState(poll.name);
+  const [endDate, setEndDate] = useState(poll.endsAt?.toISOString().split('T')[0] ?? '');
+
+  const now = new Date(new Date().setMinutes(new Date().getMinutes() + 30));
+
   return (
     <Fragment>
-      <div className={style['header']}>
-        <div className={style['title']}>
-          {poll.name}
-        </div>
-        <div 
-          className={
-            style['status'] + ' ' + 
-            (!poll.isOpen ? style['released'] : style['not-released'])
-          }>
-          {!poll.isOpen ? 'Released' : 'Not Released'}
-        </div>
-        <div className={style['space']}></div>
-        <ProgressBar 
-          progress={Math.trunc(Math.random()*100)} 
-          title={'Poll Progress'} 
-          type={'normal'}
+      <div className={
+        style['header'] + ' ' +
+        (poll.isOpen ? style['open-poll-header'] : '') 
+      }>
+        {
+          poll.isOpen ? (
+            <button className={style['release-poll-btn']}>START POLL</button>
+          ) : null
+        }
+        <PollNameInput 
+          init={poll.name} 
+          onChange={value => setPollName(value)}
+          updatable={true && !!poll.isOpen} // this should be based on the ownership of the poll
         />
-        <div className={style['end-date']}>
-          <div>Ends At: </div>
-          <div>
-            {new Date().toDateString() + ' ' + new Date().toLocaleTimeString([], {timeStyle: 'short'})}
-          </div>
-        </div>
+        <div className={style['space']}></div>
+        {
+          poll.isOpen ? null : (
+            <ProgressBar 
+              progress={Math.trunc(Math.random()*100)} 
+              title={'Poll Progress'} 
+              type={'normal'}
+            />
+          )
+        }
+        {
+          poll.isOpen ? (
+          <div className={style['end-date-picker']}>
+            <div>Ends At: </div>
+            <input 
+              type='datetime-local' 
+              value={endDate} 
+              min={`${now.getFullYear()}-${now.getMonth() < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1}-${now.getDate()}T${now.getHours()}:${now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes()}`}
+              max={new Date(new Date().setMonth(new Date().getMonth() + 1)).toISOString().split(':', 2).join(':')} 
+              onChange={e => setEndDate(e.target.value)}
+            />
+          </div>) : 
+          (
+            <div className={style['end-date']}>
+              <div>Ends At: </div>
+              <div>
+                {new Date().toDateString() + ' ' + new Date().toLocaleTimeString([], {timeStyle: 'short'})}
+              </div>
+            </div>
+          )
+        }
       </div>
     </Fragment>
   )
 };
 
-const Movie: React.FC<{movie: IMovie, progress: number, votes: number}> = ({movie, progress, votes}) => {
+const Movie: React.FC<{
+  movie: IMovie,
+  progress: number,
+  votes: number,
+  isOpenPoll: boolean,
+  pollId: number,
+  voted: VOTE_STATUS,
+  onVote: () => void}> = ({movie, progress, votes, isOpenPoll, pollId, voted, onVote}) => {
   const [loadedPoster, setLoadedPoster] = useState(false);
   const [cast, setCast] = useState<string[]>([]);
   const [duration, setDuration] = useState<string>('');
   const [director, setDirector] = useState<string>('');
   const [certification, setCertificaction] = useState<string>('');
   const [providers, setProviders] = useState<any>();
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [movieVoted, setMovieVoted] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    setShowSpinner(true);
+    dispatch(deleteMovie({movieId: movie.id, pollId: pollId}));
+  }
+
+  const handleVote = () => {
+    onVote();
+    setMovieVoted(true);
+    // dispatch(somethig);
+  }
 
   useEffect(() => {
     const getDetails = async (id: number) => {
@@ -186,15 +286,38 @@ const Movie: React.FC<{movie: IMovie, progress: number, votes: number}> = ({movi
               }
             </div>
           </div>
-          <div className={style['movie-poll-result']}>
-            <div className={style['movie-poll-result-title']}>
-              Poll result for this movie: 
-            </div>
-            <MovieResult 
-              progress={Math.trunc(Math.random()*100)}
-              votes={Math.trunc(Math.random()*100)} 
-            /> {/* change mock value by progress parameter */}
-          </div>
+          {
+            isOpenPoll ? (
+              <button className={style['delete-movie-btn']} onClick={handleDelete}>
+                  {!showSpinner ? 'Remove' : <Spinner color={'red'} />}
+              </button>
+            ) : voted === VOTE_STATUS.NOT_FETCHED ? null : voted === VOTE_STATUS.VOTED ? (
+              <div className={style['movie-poll-result']}>
+                <div className={style['movie-poll-result-title']}>
+                  Poll result for this movie: 
+                </div>
+                <MovieResult 
+                  progress={Math.trunc(Math.random()*100)}
+                  votes={Math.trunc(Math.random()*100)} 
+                /> {/* change mock value by progress parameter */}
+              </div>   
+            ) : (
+              <button 
+                className={
+                  style['vote-movie-button'] + ' ' +
+                  (voted === VOTE_STATUS.VOTING && !movieVoted ? style['disabled'] : '') 
+                } 
+                onClick={handleVote}
+                disabled={voted === VOTE_STATUS.VOTING}
+              >
+                {
+                  voted === VOTE_STATUS.VOTING && movieVoted ? (
+                    <Spinner color={'white'}/>
+                  ) : 'VOTE FOR THIS MOVIE'
+                }
+              </button>
+            )
+          }
         </div>
       </div>
     </div>
@@ -220,6 +343,11 @@ const Poll: React.FC = () => {
   const polls = useSelector(pollsSelector);
   const location = useLocation(); 
   const [totalVotes, setTotalVotes] = useState(0);
+  const [voted, setVoted] = useState<VOTE_STATUS>(VOTE_STATUS.NOT_FETCHED); // change to NOT_FETCHED
+
+  // when this component loads it does with a token
+  // if the token is used then the poll must be considered 
+  // as voted by the user.
 
   useEffect(() => {
     if (polls && location.search) {
@@ -248,7 +376,7 @@ const Poll: React.FC = () => {
       }
     >
       {
-        !poll ? null : (
+        !poll || !poll.id ? null : (
           <div className={style['poll-container']}>
             <Header poll={poll}/>
             <div className={style['movies-container']}>
@@ -259,6 +387,10 @@ const Poll: React.FC = () => {
                     movie={movie.movie as IMovie} 
                     progress={totalVotes === 0 ? 0 : Math.ceil(movie.voteCount ?? 0 / totalVotes)}
                     votes={movie.voteCount ?? 0}
+                    isOpenPoll={!!poll.isOpen}
+                    pollId={poll.id as number}
+                    voted={voted}
+                    onVote={() => setVoted(VOTE_STATUS.VOTING)}
                   />
                 </Fragment>
               ))
