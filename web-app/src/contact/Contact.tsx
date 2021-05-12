af4153c2b9aca7c452e36281c0e65826b6048569
Edwin <edwin_m_paz@hotmail.com>
2021-05-12 17:00:43 -0600

    Create Contact component

---
+++ web-app/src/contact/Contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Contact.module.scss';
+
+const Contact: React.FC = () => {
+  return (
+    <div className={style['sliders-container']}>
+      <div className={style['contact-component']}>
+        Contact Works!!
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
