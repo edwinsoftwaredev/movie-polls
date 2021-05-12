fb72aad738c30fa2b93f3f1232fd1ebaf82a1f5c
Edwin <edwin_m_paz@hotmail.com>
2021-05-12 16:58:11 -0600

    Update FirebaseUI component

--- web-app/src/auth/firebaseui/FirebaseUI.tsx
+++ web-app/src/auth/firebaseui/FirebaseUI.tsx
@@ -10,8 +10,8 @@ const uiConfig = {
   signInSuccessUrl: process.env.REACT_APP_REDIRECT_URI,
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
   ]
 };
