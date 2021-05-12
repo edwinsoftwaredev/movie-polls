a96ddd4ced2c9847df7e990e9e1ad8283c6d7073
Edwin <edwin_m_paz@hotmail.com>
2021-05-12 16:57:47 -0600

    Update Auth component

--- web-app/src/auth/Auth.tsx
+++ web-app/src/auth/Auth.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import style from './Auth.module.scss';
 import FirebaseUI from './firebaseui/FirebaseUI';
 
 const Auth: React.FC = () => {
+	const history = useHistory();
 	return (
 		<div className={style['auth-component']}>
 			<div className={style['header']}>
-				<div className={style['title']}>
+				<div 
+					onClick={e => history.push('/')}
+					className={style['title']}
+				>
 					Movie Polls
 				</div>
 				<div className={style['subtitle']}>
